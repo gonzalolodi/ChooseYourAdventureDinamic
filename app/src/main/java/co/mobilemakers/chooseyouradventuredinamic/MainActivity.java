13b90c7878c7e00491bb31a4bc83ab9be816e7c2
Gonzalo Lodi <gonzalo.lodi@globant.com>
2015-01-28 19:08:52 -0300

    StartFragment and AlleyFragment almost done

---
+++ app/src/main/java/co/mobilemakers/chooseyouradventuredinamic/MainActivity.java
@@ -0,0 +1,69 @@
+package co.mobilemakers.chooseyouradventuredinamic;
+
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
+
+
+public class MainActivity extends ActionBarActivity {
+    Button mButtonStart;
+
+    final static String FUTURE_TAG="future destination";
+    final static int ALLEY=0;
+    final static int ROOM=1;
+    final static int WIN=2;
+    final static int LOST=3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (getIntent().getExtras()==null) {
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction().
+                    add(R.id.frame_main, new StartFragment()).commit();
+        }else{
+            int future= getIntent().getIntExtra(FUTURE_TAG,0);
+            switch (future){
+                case ALLEY:
+                    FragmentManager fragmentManager = getFragmentManager();
+                    fragmentManager.beginTransaction().
+                            add(R.id.frame_main, new AlleyFragment()).commit();
+            }
+        }
+
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
---
+++ app/src/main/java/co/mobilemakers/chooseyouradventuredinamic/StartFragment.java
@@ -0,0 +1,60 @@
+package co.mobilemakers.chooseyouradventuredinamic;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.Random;
+
+public class StartFragment extends Fragment {
+    Button mButtonStart;
+
+    public StartFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_start, container, false);
+        return inflater.inflate(R.layout.fragment_start, container, false);
+
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        prepareButton();
+    }
+
+    private void prepareButton() {
+        mButtonStart= (Button) getView().findViewById(R.id.button_start);
+        mButtonStart.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                                Random random= new Random();
+                                                int future= random.nextInt(2);
+                                                switch (future){
+                                                    case 0:
+                                                        Intent alley= new Intent(getActivity(),MainActivity.class);
+                                                        alley.putExtra(MainActivity.FUTURE_TAG,MainActivity.ALLEY);
+                                                        startActivity(alley);
+                                                        break;
+                                                    case 1:
+                                                        Intent room= new Intent(getActivity(),MainActivity.class);
+                                                        startActivity(room);
+                                                        break;
+                                                }
+
+                                            }
+                                        }
+        );
+    }
+}
